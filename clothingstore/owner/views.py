7a4b7e330bba3f1ce92f05f83956a1d810f6e5ce
waseans <115150143+waseans@users.noreply.github.com>
2025-10-02 17:03:49 +0530

    added tags by clauch

--- clothingstore/owner/views.py
+++ clothingstore/owner/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+
+
+
--- clothingstore/user/tests.py
+++ clothingstore/user/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+import os
+from dotenv import load_dotenv
+from twilio.rest import Client
 
-# Create your tests here.
+# Load .env
+load_dotenv()
+
+# Initialize Twilio client
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+client = Client(account_sid, auth_token)
+
+TWILIO_NUMBER = os.getenv("TWILIO_WHATSAPP_FROM")
+
+def send_test_message(to_number):
+    try:
+        message = client.messages.create(
+            from_=TWILIO_NUMBER,
+            to=f"whatsapp:+91{to_number}",
+            body="Greetings from Clauch Factory"
+        )
+        print("Message sent successfully! SID:", message.sid)
+        return True
+    except Exception as e:
+        print("Error sending message:", e)
+        return False
+
+send_test_message("7028885969")
